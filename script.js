46061c9e78b47ff59d7248ee8333e61c3f976a7f
Rijwanul <rijwanulhoque@gmail.com>
2021-01-30 20:34:44 +0600

    Done

--- script.js
+++ script.js
@@ -24,12 +24,45 @@ function calculateTotal(){
     updateInfo('total', total);
 }
 
+
 //Function for getting quantity of a ticket class
-function getValue (ticketClass){
-   return parseInt(document.getElementById(ticketClass).value)
+function getValue (idName){
+   return parseInt(document.getElementById(idName).value);
 }
+
+//Function for getting value
+function getInfo (idName){
+    var fromInput = document.getElementById(idName).value;
+    if (fromInput != undefined){
+        return fromInput;
+    }
+    else {
+        return document.getElementById(idName).innerText;
+    }
+}
+
 
 //Function for updating InnerText
 function updateInfo(idName, value){
     document.getElementById(idName).innerText = value;
+}
+
+
+//Function for showing booking confirmation
+function bookingConfirmation(){
+    var firstClassQuantity = getInfo("firstClass");
+    var economyQuantity = getInfo("economy");
+    if (firstClassQuantity == 0 && economyQuantity == 0){
+        alert("Please buy at least one ticket!");
+        return "Ticket quantity 0 is not acceptable";
+    }
+    updateInfo("showTravelTo", getInfo("travelTo"));
+    updateInfo("showDepartureFrom", getInfo("departureFrom"));
+    updateInfo("showFirstClass", firstClassQuantity);
+    updateInfo("showEconomy", economyQuantity);
+    updateInfo("showSubtotal", getInfo("subtotal"));
+    updateInfo("showVat", getInfo("vat"));
+    updateInfo("showTotal", getInfo("total"));
+    document.getElementById('bookingArea').style.display = "none";
+    document.getElementById('confirmation').style.display = "block";
 }
